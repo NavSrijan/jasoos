f36cb76c738f496265db8fa508049695abc15566
Nav Srijan <nav.srijan.official@gmail.com>
2022-05-05 16:41:30 +0530

    Added initial comments

---
+++ main.py
@@ -0,0 +1,14 @@
+###############
+# 05-May-2022 #
+###############
+#  INTRO BOT  #
+#   Jasoos    #
+###############
+"""
+Name
+Age
+Gender
+Location ---> State, if Indian else Country
+Hobbies
+
+"""
