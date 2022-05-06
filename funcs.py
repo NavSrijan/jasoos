2750a069f4233fb93c4dc8778d23de5c07c08f2b
Nav Srijan <nav.srijan.official@gmail.com>
2022-05-06 07:15:13 +0530

    Added the basic functions

---
+++ funcs.py
@@ -0,0 +1,44 @@
+from discord import Embed
+import pdb
+class Person():
+    def __init__(self, author):
+        self.state = 0
+        
+        self.id = author.id
+        self.name = ""
+        self.age = ""
+        self.gender = ""
+        self.hobbies = ""
+    def returnEmbed(self):
+        my = Embed(title=self.name, description="f{self.age}\n{self.gender}\n{self.location}",color=0xf1c40f)
+        return my
+
+def takeIntro(p1,msg=""):
+    state = p1.state
+    reply = ""
+    if state == 0:
+        p1.state+=1
+        reply = ("What is your name?")
+        response = True
+    elif state == 1:
+        p1.name = msg.content
+        reply = "Are you 18+ or 18-?"
+        p1.reaction = True
+        response = True
+        p1.state+=1
+    elif state == 2:
+        p1.age = msg.content
+        reply = "Gender?"
+        response = True
+        p1.state+=1
+    elif state == 3:
+        p1.gender = msg.content
+        reply = "Location?"
+        response = True
+        p1.state+=1
+    elif state == 4:
+        response = False
+        p1.location = msg.content
+        p1.state+=1
+
+    return p1,reply,response
--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 #  INTRO BOT  #
 #   Jasoos    #
 ###############
+
+import discord
+from funcs import Person,takeIntro
+import configparser 
+import pdb
+
 """
 Name
 Age
@@ -12,3 +18,76 @@ Location ---> State, if Indian else Country
 Hobbies
 
 """
+client = discord.Client()
+
+config = configparser.ConfigParser()
+config.read("config")
+commands = config["commands"]
+
+# Variabels
+people = []
+waitForReaction = False
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    global waitForReaction
+    if message.author == client.user:
+        return
+
+    if message.content==commands["start"]:
+        # Checking if this person is already in an instance of a running intro.
+        alreadyPresent = False
+        for i in people:
+            if i.id==message.author.id:
+                alreadyPresent = True
+        # If the person is new and hasn't started the intro process yet.
+        if alreadyPresent==False:
+            await message.reply("starting")
+            p1 = Person(message.author)
+            p,reply,response = takeIntro(p1,msg=message)
+            people.append(p)
+            
+        else:
+            pass
+        await message.reply(reply)
+    else:
+        # People who are in mid-way process.
+        for i in people:
+            if i.id == message.author.id:
+                if waitForReaction==False:
+                    people[people.index(i)], reply, response = takeIntro(i,msg=message)
+                    # If response is required.
+                    if response==True:
+                        await message.reply(reply)
+                    elif response == "Reaction":
+                        waitForReaction = True
+                    else:
+                        embed = i.returnEmbed()
+                        await message.channel.send(embed=embed)
+                return
+        # If someone messages in between when someone is mid-way.
+        await message.reply(f"You can start your own intro process by typing {commands['start']}.")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    global waitForReaction
+    if user == client.user:
+        return
+
+    emoji = reaction.emoji
+    for i in people:
+        if i.id == reaction.message.author.id:    
+            people[people.index(i)], reply, response = takeIntro(i,msg=message)
+            # If response is required.
+            waitForReaction = False
+            if response==True:
+                await message.reply(reply)
+            elif response == "Reaction":
+                waitForReaction = True
+
+        
+client.run(config['credentials']['token'])
